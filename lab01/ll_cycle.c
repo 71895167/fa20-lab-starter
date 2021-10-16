8fca4f6a9e316bc43e7413c2159aa387b1ccb588
Lianyx <LiianwYuuX@outlook.com>
2021-10-16 15:54:11 +0800

    finish lab1

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,13 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tortoise = head, *hare = head;
+
+    while (tortoise != NULL && tortoise->next != NULL) {
+        tortoise = tortoise->next->next;
+        hare = hare->next;
+        if (hare == tortoise) 
+            return 1;
+    }
     return 0;
 }
